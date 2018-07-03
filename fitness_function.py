a6151aaf7837d638e091dcd41affb362e38704c3
XingXudong <36234770+XingXudong@users.noreply.github.com>
2018-07-03 21:42:11 +0800

    Add files via upload

---
+++ fitness_function.py
@@ -0,0 +1,41 @@
+"""
+	适应度值函数管理（类）
+	编写人：邢旭东
+	邮箱：xd_xing@sina.com
+	时间：2018.06.29
+	版本：v0.1
+"""
+
+import numpy as np
+
+
+class FitnessFunction:
+	"""
+		适应度值函数管理系统
+	"""
+
+	def __init__(self, dimension):
+		"""
+		类构造方法
+		:param dimension: 特征维度
+		"""
+		self.dimension = dimension
+
+	def rastrigin(self, position):
+		"""
+		Rastrigin函数
+		:param position: 函数自变量
+		:return: 适应度值
+		"""
+		fitness_value = -10 * self.dimension * \
+			np.exp(-0.2 * (np.sum(position ** 2) / 2) ** 0.5) - \
+			np.exp(np.sum(np.cos(2 * np.pi * position)) / 2) + \
+			10 * self.dimension + np.exp(1)
+		return fitness_value
+
+
+if __name__ == '__main__':
+	fun = FitnessFunction(2)
+	val = fun.rastrigin(np.array([0, 0]))
+	print(val)
+
---
+++ pso_main_script.py
@@ -0,0 +1,69 @@
+"""
+	PSO算法的主逻辑脚本
+	编写人：邢旭东
+	邮箱：xd_xing@sina.com
+	时间：2018.06.29
+	版本：v0.1
+"""
+
+import numpy as np
+import fitness_function as ff
+import standard_particle as sp
+import matplotlib.pyplot as plt
+
+dimension = 2   # 优化问题特征维度
+
+# 算法超参数设置
+c1 = 2
+c2 = 2
+iteration_num = 50
+particle_num = 100
+max_velocity = 0.005
+min_velocity = 0.001
+max_position = 50
+min_position = -50
+
+# 种群初始化
+particle_swarm = []
+for i in range(particle_num):
+	particle_swarm.append(sp.Particle(dimension, max_position, min_position, max_velocity, min_velocity))
+
+# 适应度值计算
+fitness_function = ff.FitnessFunction(dimension)    # 创建适应度函数对象
+fitness_value = []  # 适应度值
+personal_best = []  # 个体历史最优位置
+fitness_personal_best = []  # 个体历史最优适应度值
+for particle in particle_swarm:
+	fitness_value.append(fitness_function.rastrigin(particle.position))
+	personal_best.append(particle.position)
+	fitness_personal_best.append(fitness_function.rastrigin(particle.position))
+
+global_best = personal_best[int(np.argmax(fitness_personal_best))]  # 种群全局最优位置
+fitness_global_best = np.amax(fitness_personal_best)    # 种群全局最优适应度值
+
+yy = []
+for i in range(iteration_num):
+
+	for particle_index, particle in enumerate(particle_swarm):
+		particle.update_velocity(c1, c2, personal_best[particle_index], global_best)    # 速度更新
+		particle.update_position()  # 位置更新
+		particle.correction()   # 搜索范围和搜索速度修正
+		fitness_value[particle_index] = fitness_function.rastrigin(particle.position)   # 适应度值更新
+
+	for particle_index, particle in enumerate(particle_swarm):
+		if fitness_value[particle_index] < fitness_personal_best[particle_index]:
+			fitness_personal_best[particle_index] = fitness_value[particle_index]
+			personal_best[particle_index] = particle.position
+		if fitness_value[particle_index] < fitness_global_best:
+			fitness_global_best = fitness_value[particle_index]
+			global_best = particle.position
+
+	yy.append(fitness_global_best)
+
+# 结果可视化
+plt.figure()
+plt.plot(range(1, iteration_num + 1), yy)
+plt.xlabel('iterate')
+plt.ylabel('fitness value')
+plt.title('PSO algorithm for 2D Rastrigin')
+plt.show()
